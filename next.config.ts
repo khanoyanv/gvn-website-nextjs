f4eae7593a9f55cec86b56a4b39ead9a9b5d1b0a
armhunanyan <arman@hunanyan.am>
2024-12-12 22:28:03 +0400

    config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
