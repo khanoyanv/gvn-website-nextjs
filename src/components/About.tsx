a4dadb3e8db93a8e41b25b87650af563113f656d
vahe.khanoyan <vahe.khanoyan@picsart.academy>
2024-12-13 20:56:55 +0400

    description and representative part fixes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,13 +15,17 @@ export const About = () => {
           className={"description"}
           // style={{ color: "#1f509a", fontSize: "24px" }}
         >
-          GVN PRO company was founded in 2019 and is actively operating in
+          GVN Pro company was founded in 2019 and is actively operating in
           Yerevan, it specializes in professional audio, photo and video, media
-          equipment supply and sales. The company cooperates with the leading
-          companies, manufacturers, offers and supports such brands as Sony,
-          Panasonic, Samsung, Canon, Manfrotto, Libec, DJI, Blackmagic,
-          Sennheiser, Aja, Canare, Neutrik etc. The GVN PRO is the supplier for
-          the leading broadcast and media companies in Armenia.
+          equipment supply and sales. GVN Pro is the official representative of{" "}
+          <a href="http://www.univiso.tv/en/index.html" target="_blank" className='descriptionLink'>
+            Univiso Technologies & Develop Ltd.
+          </a>{" "}
+          The company cooperates with the leading companies, manufacturers,
+          offers and supports such brands as Sony, Panasonic, Samsung, Canon,
+          Manfrotto, Libec, DJI, Blackmagic, Sennheiser, Aja, Canare, Neutrik
+          etc. The GVN Pro is the supplier for the leading broadcast and media
+          companies in Armenia.
         </p>
       </div>
       {/* <div className={"imageContainer"}>
