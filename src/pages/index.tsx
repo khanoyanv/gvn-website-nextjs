e2c6dc6e7637165bb417c69d4b972c4b1cde65ab
vahe.khanoyan <vahe.khanoyan@picsart.academy>
2025-06-12 13:13:40 +0400

    fixed tab icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import { Creator } from "../components/Creator";
-import { Hero } from "../components/Hero";
-import { Products } from "../components/Products";
-import { Contacts } from "../components/Contacts";
-import { About } from "../components/About";
-import Head from "next/head";
+import { Creator } from '../components/Creator';
+import { Hero } from '../components/Hero';
+import { Products } from '../components/Products';
+import { Contacts } from '../components/Contacts';
+import { About } from '../components/About';
+import Head from 'next/head';
 
 const MainPage = () => {
   return (
     <>
       <Head>
         <title>GVN Pro - Capture Your Vision</title>
-        <link rel="icon" href="/gvnpro.png" />
+        <link rel="icon" href="/tabIcon.png" />
       </Head>
       <Hero />
       <Creator />
